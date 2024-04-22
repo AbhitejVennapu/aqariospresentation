541798bf392d4cae8fd1e1e0e103843b00ccdd98
Abhitej Vennapu <vennapu@rhrk.uni-kl.de>
2024-04-22 09:31:08 +0200

    Tool tips

--- presentation/src/components/IconTransition.jsx
+++ presentation/src/components/IconTransition.jsx
@@ -1,19 +1,21 @@
-import { Stack, Typography } from "@mui/material"
+import { Stack, Tooltip, Typography } from "@mui/material"
 import React from "react"
 import { useIconTransition } from "../useIconTransition"
 
 const IconTransition = ({ list, speed = 500 }) => {
     const imageList = useIconTransition(list, speed)
-
+    const labeList = ["React", "MUI", "Jest", "Git", "Redux", "YJs"]
     return (
         <Stack direction="row" spacing={2.5}>
             {imageList.map((image, index) => (
-                <img
-                    key={index}
-                    height={window.innerWidth < 600 ? 32 : 48}
-                    alt="icons"
-                    src={image}
-                />
+                <Tooltip title={labeList[index]}>
+                    <img
+                        key={index}
+                        height={window.innerWidth < 600 ? 32 : 48}
+                        alt="icons"
+                        src={image}
+                    />
+                </Tooltip>
             ))}
         </Stack>
     )
